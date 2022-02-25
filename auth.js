e60505518ad06f9572a4f5efdcd4980e58739c44
Polo <poloaguirre3421@gmail.com>
2022-02-25 16:57:51 -0700

    Initial Commit

--- auth.js
+++ auth.js
@@ -1,20 +1,17 @@
 // Load required packages
-var passport = require('passport');
-var BasicStrategy = require('passport-http').BasicStrategy;
+var passport = require("passport");
+var BasicStrategy = require("passport-http").BasicStrategy;
 
-passport.use(new BasicStrategy(
-    function(username, password, done) {
-        //hard coded
-        var user = { name: "testuser" };
-        if (username == user.name && password == "cu")
-        {
-            return done(null, user);
-        }
-        else
-        {
-            return done(null, false);
-        }
+passport.use(
+  new BasicStrategy(function (username, password, done) {
+    //hard coded
+    var user = { name: "testuser" };
+    if (username == user.name && password == "cu") {
+      return done(null, user);
+    } else {
+      return done(null, false);
     }
-));
+  })
+);
 
-exports.isAuthenticated = passport.authenticate('basic', { session : false });
+exports.isAuthenticated = passport.authenticate("basic", { session: false });
